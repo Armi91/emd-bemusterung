4d9b997f8f73792acf14dace2582ef8487e4dbae
Dawid Armatys <daw.armatys@gmail.com>
2023-09-06 21:51:40 +0200

    Fixed dependencies

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Auth, AuthError, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "@angular/fire/auth";
+import {
+  Auth,
+  AuthError,
+  authState,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from '@angular/fire/auth';
 import { doc, docData, Firestore, setDoc } from '@angular/fire/firestore';
 import { Observable, switchMap, of, catchError } from 'rxjs';
 import { UserData } from '../interfaces/user-data.interface';
@@ -7,58 +14,64 @@ import { Store } from '@ngrx/store';
 import { setUser } from './auth.actions';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   user$!: Observable<UserData | null>;
 
   constructor(
     private auth: Auth,
     private firestore: Firestore,
-    private store: Store
+    private store: Store,
   ) {
     this.getCurrentUser().subscribe((user) => {
-      if(user) {
-        this.store.dispatch(setUser({user}))
+      if (user) {
+        this.store.dispatch(setUser({ user }));
       } else {
-
       }
-    })
+    });
   }
 
   getCurrentUser() {
     return authState(this.auth).pipe(
-      switchMap(user => {
+      switchMap((user) => {
         if (user) {
-          return <Observable<UserData>>docData(doc(this.firestore, `users/${user.uid}`));
+          return <Observable<UserData>>(
+            docData(doc(this.firestore, `users/${user.uid}`))
+          );
         } else {
           return of(null);
         }
       })
-    )
+    );
   }
 
   async register(userForm: UserData) {
-    return createUserWithEmailAndPassword(this.auth, userForm.email, userForm.password!).then(userCredential => {
-      delete userForm.password;
-      const userRef = doc(this.firestore, `users/${userCredential.user.uid}`)
-      return setDoc(userRef, {...userForm, uid: userCredential.user.uid})
-    }).catch((err: AuthError) => {
-      console.log(err.code);
-      return err;
-    })
+    return createUserWithEmailAndPassword(
+      this.auth,
+      userForm.email,
+      userForm.password!
+    )
+      .then((userCredential) => {
+        delete userForm.password;
+        const userRef = doc(this.firestore, `users/${userCredential.user.uid}`);
+        return setDoc(userRef, { ...userForm, uid: userCredential.user.uid });
+      })
+      .catch((err: AuthError) => {
+        console.log(err.code);
+        return err;
+      });
   }
 
   loginWithEmailAndPassword(email: string, password: string) {
-    return signInWithEmailAndPassword(this.auth, email, password)
+    return signInWithEmailAndPassword(this.auth, email, password);
   }
 
   getUserData(uid: string): Observable<UserData> {
-    return <Observable<UserData>>docData(doc(this.firestore, `users/${uid}`))
+    return <Observable<UserData>>docData(doc(this.firestore, `users/${uid}`));
   }
 
   logout() {
-    signOut(this.auth)
+    signOut(this.auth);
   }
 }
--- src/app/client/client-routing.module.ts
+++ src/app/client/client-routing.module.ts
@@ -8,6 +8,12 @@ const routes: Routes = [
     path: '',
     component: ClientComponent,
     canActivate: [AuthGuard],
+    children: [
+      // {
+      //   path: 'init',
+      //   component: ClientComponent
+      // }
+    ]
   }
 ];
 
